8055ea263b51271d2551d97d6fd01ab6addf3e2f
kirill <shaposhnikov.ks@gmail.com>
2017-10-01 23:58:41 +0400

    fixup! Task 4

--- signature/src/com/shaposhnikov/Main.java
+++ signature/src/com/shaposhnikov/Main.java
@@ -7,10 +7,10 @@ import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -29,36 +29,40 @@ public class Main {
     }
 
     private static List<String> findCopies(File directory, final byte[] signature) {
-        if (directory.isFile()) {
-            throw new RuntimeException("Was provided file instead of directory");
+        List<String> result = new ArrayList<>();
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                result.addAll(findCopies(file, signature));
+            } else {
+                try (RandomAccessFile accessFile = new RandomAccessFile(file, "r");
+                     FileChannel channel = accessFile.getChannel()) {
+                    ByteBuffer part1 = ByteBuffer.allocate(16);
+                    part1.put(Arrays.copyOfRange(signature, 0, 16));
+                    part1.position(0);
+                    ByteBuffer part2 = ByteBuffer.allocate(16);
+                    part2.put(Arrays.copyOfRange(signature, 16, signature.length));
+                    part2.position(0);
+
+                    ByteBuffer buffer = ByteBuffer.allocate(16);
+                    int res = 0;
+                    while (channel.read(buffer) != -1) {
+                        buffer.position(0);
+                        if (part1.equals(buffer) || part2.equals(buffer)) {
+                            res++;
+                        }
+                        buffer.clear();
+                    }
+
+                    if (res == 2) {
+                        result.add(file.getAbsolutePath());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
-        return Arrays.stream(directory.listFiles()).filter(file -> {
-            try (RandomAccessFile accessFile = new RandomAccessFile(file, "r");
-                 FileChannel channel = accessFile.getChannel()) {
-                ByteBuffer part1 = ByteBuffer.allocate(16);
-                part1.put(Arrays.copyOfRange(signature, 0, 16));
-                part1.position(0);
-                ByteBuffer part2 = ByteBuffer.allocate(16);
-                part2.put(Arrays.copyOfRange(signature, 16, signature.length));
-                part2.position(0);
-
-                ByteBuffer buffer = ByteBuffer.allocate(16);
-                int res = 0;
-                while (channel.read(buffer) != -1) {
-                    buffer.position(0);
-                    if (part1.equals(buffer) || part2.equals(buffer)) {
-                        res++;
-                    }
-                    buffer.clear();
-                }
-
-                return res == 2;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return false;
-        }).map(File::getAbsolutePath).collect(Collectors.toList());
+        return result;
     }
 
     private static byte[] getSignature(File file) throws IOException {
